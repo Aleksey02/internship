9fc185788ee49960c463d464ff47169486ba3234
Алексей <lyosha2002stich@gmail.com>
2023-03-03 18:37:11 +0500

    first commit

---
+++ Conquest/conquest/src/App.vue
@@ -0,0 +1,155 @@
+<script setup>
+</script>
+
+<template>
+  <div class="body">
+    <header class="header">
+      <div class="container">
+        <nav class="header__nav">
+          <div class="header__nav__logo">
+            <img src="./assets/images/logo.png" alt="" class="header__nav__logo-img">
+            <span class="header__nav__logo-text">CONQUEST</span>
+          </div>
+          <ul class="header__nav__list">
+            <li class="header__nav__list-link">Каталог</li>
+            <li class="header__nav__list-link">Акции</li>
+            <li class="header__nav__list-link">Доставка</li>
+            <li class="header__nav__list-link">Контакты</li>
+          </ul>
+          <div class="header__nav__btns">
+            <img src="./assets/images/search.svg" alt="" class="header__nav__btns-btn">
+            <img src="./assets/images/heart.svg" alt="" class="header__nav__btns-btn">
+            <img src="./assets/images/cart.svg" alt="" class="header__nav__btns-btn">
+          </div>
+        </nav>
+      </div>
+    </header>
+    <main class="main">
+      <div class="main__top">
+        <div class="container main__top__box">
+          <div class="main__top__left">
+            <h2 class="main__top__left-title1">Весна/Лето 2019</h2>
+            <h2 class="main__top__left-title2">Коллекция</h2>
+            <p class="main__top__left-text">Швейцарские часы в наличии в Москве и с доставкой по всему миру</p>
+            <button class="main__top__left-btn">Смотреть каталог</button>
+          </div>
+          <div class="main__top__right">
+            <img src="./assets/images/watch.png" alt="" class="main__top__right-watch">
+            <img src="./assets/images/bgSphere.png" alt="" class="main__top__right-bg">
+          </div>
+        </div>
+      </div>
+      <div class="main__models">
+        <div class="container">
+          <h2 class="main__models-title">Популярные модели</h2>
+          <div class="main__models-look">Смотреть все</div>
+          <div class="main__models__box">
+            <div class="main__models__box__item">
+              <div class="main__models__box__item-title">RADO</div>
+              <div class="main__models__box__item-price">65 300 ₽</div>
+              <div class="main__models__box__item-image">
+                <img src="./assets/images/watch_1.png" alt="" class="main__models__box__item-image-img">
+                <img src="./assets/images/watchBg.png" alt="" class="main__models__box__item-image-bg">
+              </div>
+              <div class="main__models__box__item-link">Подробнее</div>
+            </div>
+            <div class="main__models__box__item">
+              <div class="main__models__box__item-title">BVLGARI</div>
+              <div class="main__models__box__item-price">81 700 ₽</div>
+              <div class="main__models__box__item-image">
+                <img src="./assets/images/watch_3.png" alt="" class="main__models__box__item-image-img">
+                <img src="./assets/images/watchBg.png" alt="" class="main__models__box__item-image-bg">
+              </div>
+              <div class="main__models__box__item-link">Подробнее</div>
+            </div>
+            <div class="main__models__box__item">
+              <div class="main__models__box__item-title">BVLGARI</div>
+              <div class="main__models__box__item-price">72 700 ₽</div>
+              <div class="main__models__box__item-image">
+                <img src="./assets/images/watch_4.png" alt="" class="main__models__box__item-image-img">
+                <img src="./assets/images/watchBg.png" alt="" class="main__models__box__item-image-bg">
+              </div>
+              <div class="main__models__box__item-link">Подробнее</div>
+            </div>
+          </div>
+      </div>
+      </div>
+      <div class="main__catalog">
+        
+          <div class="main__catalog__left">
+            <img src="./assets/images/photo_banner.jpg" alt="" class="main__catalog__left-img">
+          </div>
+          <div class="main__catalog__right">
+            <div class="main__catalog__right-box">
+              <div class="main__catalog__right-title">Подлинное качество</div>
+              <div class="main__catalog__right-text">Все часы производятся в Швейцарии и имеют сертификаты качества. Настоящие швейцарские часы — это предмет гордости и престижа.</div>
+              <button class="main__catalog__right-btn">СМОТРЕТЬ КАТАЛОГ</button>          
+            </div>
+          
+        </div>
+      </div>
+      <div class="main__news">
+        <div class="container">
+          <div class="main__news__box">
+            <div class="main__news__box__left">
+              <h2 class="main__news__box__left-title">Следите за нашими новостями</h2>
+              <p class="main__news__box__left-text">Подписывайтесь на <b>@conquest_watch</b> в соцсетях и узнавайте о новинках и акциях первыми.</p>
+              <div class="main__news__box__left-social">
+                <img src="./assets/images/instagram_icon.svg" alt="" class="main__news__box__left-social-img">
+                <img src="./assets/images/facebook_icon.svg" alt="" class="main__news__box__left-social-img">
+                <img src="./assets/images/twitter_icon.svg" alt="" class="main__news__box__left-social-img">
+              </div>
+            </div>
+            <div class="main__news__box__right">
+              <div class="main__news__box__right-little">
+                <img src="./assets/images/photo_1.jpg" alt="">
+                <img src="./assets/images/photo_2.jpg" alt="">
+              </div>
+              <img src="./assets/images/photo_3.jpg" alt="" class="main__news__box__right-big">
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+    <footer class="footer">
+      <div class="container">
+        <div class="footer__box">
+          <div class="footer__box__logo">
+            <div class="footer__box__logo-logo">
+              <img src="./assets/images/logoWhite.png" alt="">
+              <span>CONQUEST</span>
+            </div>
+            <div class="footer__box__logo-text">8 (499) 450-47-97</div>
+            <div class="footer__box__logo-text">info@conquest.watch.ru</div>           
+            <div class=""></div>
+          </div>
+          <div class="footer__box__info">
+            <div class="footer__box__info-title">о компании</div>
+            <div class="footer__box__info-text">Наши магазины</div>
+            <div class="footer__box__info-text">Вакансии</div>
+            <div class="footer__box__info-text">Сертификаты</div>
+            <div class="footer__box__info-text">Отзывы</div>            
+          </div>
+          <div class="footer__box__info">
+            <div class="footer__box__info-title">покупателям</div>
+            <div class="footer__box__info-text">Каталог товаров</div>
+            <div class="footer__box__info-text">Как заказать?</div>
+            <div class="footer__box__info-text">FAQ</div>
+            <div class="footer__box__info-text">Корпоративным клиентам</div>            
+          </div>
+          <div class="footer__box__info">
+            <div class="footer__box__info-title">оплата и доставка</div>
+            <div class="footer__box__info-text">Способы оплаты</div>
+            <div class="footer__box__info-text">Время доставки</div>
+            <div class="footer__box__info-text">Гарантии и ремонт</div>
+            <div class="footer__box__info-text">Возврат и компенсация</div>            
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
